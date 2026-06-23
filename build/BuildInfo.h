7b5ac673ba875b6e81b4b9411e8dfc57c482718c
Jérôme Béroud <jerome@beroud.info>
2026-06-23 08:02:14 +0200

    Bump plugin version to 1.2.1

--- build/BuildInfo.h
+++ build/BuildInfo.h
@@ -1,6 +1,6 @@
 #pragma once
 
 // Generated build information
-#define OVT_BUILD_ID "fed54fd"
-#define OVT_BUILD_TIME "2026-06-23 07:48:35"
+#define OVT_BUILD_ID "71cd85f"
+#define OVT_BUILD_TIME "2026-06-23 07:55:38"
 #define OVT_PROJECT_VERSION "1.2.0"
